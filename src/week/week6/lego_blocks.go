ffd7e2fbfd75d4c48fbad09e6e65183af7b1a907
BesAnh <anhle3532@gmail.com>
2025-01-06 16:34:03 +0700

    update - reorganize folder

---
+++ src/week/week6/lego_blocks.go
@@ -0,0 +1,13 @@
+package week6
+
+/*
+ * - The wall should not have any holes in it.
+ * - The wall you build should be one solid structure, so there should not be a straight vertical break across all rows of bricks.
+ * - The bricks must be laid horizontally.
+ * n: height, m: width
+ */
+func LegoBlocks(n, m int32) int32 {
+	// Write your code here
+
+	return 0
+}
