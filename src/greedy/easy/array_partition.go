ea76c517042094cac83cbfad6da7e94718ad6e58
BesAnh <anhle3532@gmail.com>
2025-02-09 16:23:46 +0700

    update - array partition

---
+++ src/greedy/easy/array_partition.go
@@ -0,0 +1,22 @@
+package easy
+
+import "sort"
+
+/* Approach: sort the slice and get item at position i=2,4,...
+ * Input: nums = [1,4,3,2]
+ * Output: 4
+ * Explanation: All possible pairings (ignoring the ordering of elements) are:
+ * 1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+ * 2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+ * 3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+ * So the maximum possible sum is 4.
+ */
+func ArrayPairSum(nums []int) int {
+	sort.Ints(nums)
+	sum := 0
+	for i := 0; i < len(nums); i += 2 {
+		sum += nums[i]
+	}
+
+	return sum
+}
